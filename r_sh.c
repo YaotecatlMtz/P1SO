8df1e1d8b6eb7c07289c789d91ed0b7c5399111d
Renata <102703000+codrenatat@users.noreply.github.com>
2025-02-16 15:10:59 -0600

    Update r_sh.c

--- r_sh.c
+++ r_sh.c
@@ -1,4 +1,3 @@
-// sh.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -6,22 +5,33 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void execute_command(char *command) {
+void execute_command(char *input) {
     int background = 0;
-    size_t len = strlen(command);
-    if (len > 0 && command[len - 1] == '&') {
-        background = 1;
-        command[len - 1] = '\0';
-    }
+    char *args[20];
+    int arg_count = 0;
     
+    char *token = strtok(input, " ");
+    while (token != NULL) {
+        if (strcmp(token, "&") == 0) {
+            background = 1;
+        } else {
+            args[arg_count++] = token;
+        }
+        token = strtok(NULL, " ");
+    }
+    args[arg_count] = NULL;
+    
+    if (arg_count == 0) return; 
     pid_t pid = fork();
     if (pid == 0) {
-        execlp(command, command, NULL);
-        perror("execlp");
+        execvp(args[0], args);
+        perror("execvp");
         exit(EXIT_FAILURE);
     } else if (pid > 0) {
         if (!background) {
             waitpid(pid, NULL, 0);
+        } else {
+            printf("[Proceso en segundo plano: %d]\n", pid);
         }
     } else {
         perror("fork");
@@ -29,20 +39,21 @@ void execute_command(char *command) {
 }
 
 int main() {
-    char command[100];
+    char input[100];
     while (1) {
         printf("sh> ");
-        fgets(command, sizeof(command), stdin);
-        command[strcspn(command, "\n")] = 0;
+        fgets(input, sizeof(input), stdin);
+        input[strcspn(input, "\n")] = 0;
         
-        if (strcmp(command, "exit") == 0) {
+        if (strcmp(input, "exit") == 0) {
             exit(0);
-        } else if (strcmp(command, "shutdown") == 0) {
+        } else if (strcmp(input, "shutdown") == 0) {
             system("killall getty init sh");
             exit(0);
         } else {
-            execute_command(command);
+            execute_command(input);
         }
     }
     return 0;
 }
+
