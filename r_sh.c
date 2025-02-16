633fe44d9c618b4cb6b0a78e5c699d203e2405d7
Renata <102703000+codrenatat@users.noreply.github.com>
2025-02-16 15:18:00 -0600

    Update r_sh.c

--- r_sh.c
+++ r_sh.c
@@ -1,3 +1,5 @@
+// sh.c
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -5,35 +7,36 @@
 #include <sys/wait.h>
 
 void execute_command(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        exit(0);
-    } else if (strcmp(command, "shutdown") == 0) {
-        system("killall getty init sh");
-        exit(0);
-    } else {
-        pid_t pid = fork();
-        if (pid == 0) {
-            execlp(command, command, NULL);
-            perror("execlp");
-            exit(EXIT_FAILURE);
-        } else {
-            wait(NULL);
     int background = 0;
     size_t len = strlen(command);
     if (len > 0 && command[len - 1] == '&') {
         background = 1;
         command[len - 1] = '\0';
     }
-    
+
     pid_t pid = fork();
-    if (pid == 0) {
+    if (pid == 0) { // Proceso hijo
         execlp(command, command, NULL);
-        perror("execlp");
+        perror("execlp"); // Solo llega aquí si hay error
         exit(EXIT_FAILURE);
-    } else if (pid > 0) {
+    } else if (pid > 0) { // Proceso padre
         if (!background) {
             waitpid(pid, NULL, 0);
         }
+    } else {
+        perror("fork");
+    }
+}
+
+void shutdown_system() {
+    pid_t pid = fork();
+    if (pid == 0) { // Proceso hijo
+        execlp("killall", "killall", "getty", "init", "sh", NULL);
+        perror("execlp"); // Solo llega aquí si hay error
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) { // Proceso padre
+        waitpid(pid, NULL, 0);
+        exit(0);
     } else {
         perror("fork");
     }
@@ -43,20 +46,16 @@ int main() {
     char command[100];
     while (1) {
         printf("sh> ");
-        scanf("%s", command);
-        execute_command(command);
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = 0;
-        
+
         if (strcmp(command, "exit") == 0) {
             exit(0);
         } else if (strcmp(command, "shutdown") == 0) {
-            system("killall getty init sh");
-            exit(0);
+            shutdown_system();
         } else {
             execute_command(command);
         }
     }
     return 0;
-}
 }
