36055c0c2a8dce3cc72ac7ba7234ee17d76dcfa1
Renata <102703000+codrenatat@users.noreply.github.com>
2025-02-16 15:26:25 -0600

    Update r_sh.c

--- r_sh.c
+++ r_sh.c
@@ -8,27 +8,42 @@ void execute_command(char *command) {
     if (strcmp(command, "exit") == 0) {
         exit(0);
     } else if (strcmp(command, "shutdown") == 0) {
-        system("killall getty init sh");
-        exit(0);
+        pid_t pid = fork();
+        if (pid == 0) {
+            execlp("shutdown", "shutdown", "-h", "now", NULL);
+            perror("shutdown failed");
+            exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            waitpid(pid, NULL, 0);
+        } else {
+            perror("fork");
+        }
     } else {
         pid_t pid = fork();
         if (pid == 0) {
             execlp(command, command, NULL);
             perror("execlp");
             exit(EXIT_FAILURE);
+        } else if (pid > 0) {
+            waitpid(pid, NULL, 0);
         } else {
-            wait(NULL);
+            perror("fork");
         }
     }
 }
 
 int main() {
     char command[100];
+
     while (1) {
         printf("sh> ");
-        scanf("%s", command);
+        if (fgets(command, sizeof(command), stdin) == NULL) {
+            break; // Handle EOF (Ctrl+D)
+        }
+        
+        command[strcspn(command, "\n")] = 0; // Remove newline character
         execute_command(command);
     }
-    return 0;
+
     return 0;
 }
