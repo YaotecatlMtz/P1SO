b8e4b372a3c3ef91b2f5ca57921cd5a73c7848e6
Renata <102703000+codrenatat@users.noreply.github.com>
2025-02-16 20:17:02 -0600

    Update r_sh.c

--- r_sh.c
+++ r_sh.c
@@ -2,32 +2,48 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
+
+const char *processes_to_kill[] = {"sh", "getty", "init"};
+const int num_processes = 3;
 
 void execute_command(char *command) {
     if (strcmp(command, "exit") == 0) {
         exit(0);
-    } else if (strcmp(command, "shutdown") == 0) {
-        pid_t pid = fork();
-        if (pid == 0) {
-            execlp("shutdown", "shutdown", "-h", "now", NULL);
-            perror("shutdown failed");
-            exit(EXIT_FAILURE);
-        } else if (pid > 0) {
-            waitpid(pid, NULL, 0);
-        } else {
-            perror("fork");
+    } 
+    else if (strcmp(command, "shutdown") == 0) {
+        printf("System shutting down...\n");
+
+        for (int i = 0; i < num_processes; i++) {
+            pid_t pid = fork();
+            if (pid == 0) {
+                execlp("pkill", "pkill", processes_to_kill[i], NULL);
+                perror("Error ejecutando pkill");
+                exit(EXIT_FAILURE);
+            } else if (pid > 0) {
+                waitpid(pid, NULL, 0);
+            } else {
+                perror("Error en fork");
+            }
         }
-    } else {
+
+        exit(0);
+    } 
+    else {
         pid_t pid = fork();
-        if (pid == 0) {
-            execlp(command, command, NULL);
-            perror("execlp");
+        if (pid == 0) {  
+            char *args[] = {command, NULL}; 
+            execvp(command, args);
+            perror("Error ejecutando comando");
             exit(EXIT_FAILURE);
-        } else if (pid > 0) {
+        } 
+        else if (pid > 0) {  
             waitpid(pid, NULL, 0);
-        } else {
-            perror("fork");
+        } 
+        else {  
+            perror("Error en fork");
         }
     }
 }
@@ -38,12 +54,16 @@ int main() {
     while (1) {
         printf("sh> ");
         if (fgets(command, sizeof(command), stdin) == NULL) {
-            break; // Handle EOF (Ctrl+D)
+            break;
         }
         
-        command[strcspn(command, "\n")] = 0; // Remove newline character
+        command[strcspn(command, "\n")] = 0;
+        
         execute_command(command);
     }
+
+    return 0;
+}
 
     return 0;
 }
