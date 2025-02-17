b54d78fd962f35773e724658d8fbda429f257991
Renata <102703000+codrenatat@users.noreply.github.com>
2025-02-16 20:21:55 -0600

    Update r_sh.c

--- r_sh.c
+++ r_sh.c
@@ -6,35 +6,43 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-// List of processes to terminate in shutdown
-const char *processes_to_kill[] = {"sh", "getty", "init"};
-const int num_processes = 3;
+// Function to shut down the system properly
+void shutdown_system() {
+    printf("System shutting down...\n");
 
-// Function to execute external commands
+    // Kill init first so it stops restarting processes
+    pid_t pid = fork();
+    if (pid == 0) {
+        execlp("pkill", "pkill", "-9", "init", NULL);
+        perror("Error terminating init");
+        exit(EXIT_FAILURE);
+    }
+    waitpid(pid, NULL, 0);
+
+    // Kill remaining processes
+    const char *processes_to_kill[] = {"sh", "getty"};
+    for (int i = 0; i < 2; i++) {
+        pid = fork();
+        if (pid == 0) {
+            execlp("pkill", "pkill", "-9", processes_to_kill[i], NULL);
+            perror("Error terminating process");
+            exit(EXIT_FAILURE);
+        }
+        waitpid(pid, NULL, 0);
+    }
+
+    printf("Shutdown complete. All processes terminated.\n");
+    exit(0);
+}
+
+// Function to execute commands
 void execute_command(char *command) {
     if (strcmp(command, "exit") == 0) {
         printf("Exiting shell...\n");
         exit(0);
     } 
     else if (strcmp(command, "shutdown") == 0) {
-        printf("System shutting down...\n");
-
-        // Kill all relevant processes
-        for (int i = 0; i < num_processes; i++) {
-            pid_t pid = fork();
-            if (pid == 0) {  // Child process
-                execlp("pkill", "pkill", "-9", processes_to_kill[i], NULL);
-                perror("Error executing pkill");
-                exit(EXIT_FAILURE);
-            } else if (pid > 0) {  // Parent process
-                waitpid(pid, NULL, 0);
-            } else {
-                perror("Error in fork");
-            }
-        }
-
-        printf("All processes terminated. System shutdown complete.\n");
-        exit(0);
+        shutdown_system();
     } 
     else {
         // Create a child process to execute the command
@@ -59,7 +67,7 @@ int main() {
     while (1) {
         printf("sh> ");
         if (fgets(command, sizeof(command), stdin) == NULL) {
-            break;  // Handle EOF (Ctrl+D)
+            break; // Handle EOF (Ctrl+D)
         }
 
         // Remove trailing newline
@@ -68,6 +76,9 @@ int main() {
         // Execute the command
         execute_command(command);
     }
+
+    return 0;
+}
 
     return 0;
 }
